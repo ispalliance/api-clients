667662181dc5e9da36a44c1b24941a67027f26de
Tomas Sedlacek <mail@kedlas.cz>
2018-08-30 17:59:47 +0200

    pedef: added thumbnail post method body

--- src/App/Pedef/Requestor/ThumbnailRequestor.php
+++ src/App/Pedef/Requestor/ThumbnailRequestor.php
@@ -7,9 +7,24 @@ use Psr\Http\Message\ResponseInterface;
 class ThumbnailRequestor extends BaseRequestor
 {
 
-	public function generateThumbnail(): ResponseInterface
+	public function generateThumbnail($pdfContents): ResponseInterface
 	{
-		return $this->client->post('thumbnail');
+		return $this->client->post(
+			'thumbnail',
+			[
+				'multipart' => [
+					[
+						'name' => 'PDF file',
+						'filename' => "file.pdf",
+						'contents' => $pdfContents,
+						'headers' => [
+							'Content-Type' => 'application/pdf',
+							'Content-Transfer-Encoding' => "binary",
+						],
+					],
+				],
+			]
+		);
 	}
 
 }
